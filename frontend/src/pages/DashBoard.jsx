886df3bad626fde3eb9820ee787176d28533c63a
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-01-31 18:51:20 +0530

    check runs

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function Dashboard() {
+  const navigate = useNavigate();
   return (
     <div>
       <Navbar />
+      <button
+        onClick={() => {
+          navigate("/login", { replace: true })
+        }}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      > Login</button>
     </div>
   );
 }
