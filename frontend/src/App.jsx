197437e76e0f08f2265adf759e3e7e6f08601d79
Bhargab Dhungel <bhargabdhungel@gmail.com>
2024-02-01 10:54:28 +0530

    frontend fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,17 @@
-import axios from 'axios';
 import './App.css'
-import { useEffect, useState } from 'react';
-// import { Routes, Route } from 'react-router-dom'
-// import Dashboard from './pages/DashBoard';
-// import Login from './pages/Login';
-// import Signup from './pages/Signup';
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/DashBoard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
-  const [data, setData] = useState();
-  console.log(data);
-  useEffect(() => {
-    axios.get(import.meta.env.VITE_SERVER + '/').then((res) => {
-      setData(res.data);
-    }).catch((err) => {
-      setData(err.message);
-    });
-  },[]);
   return (
     <div className='dark:bg-gray-600'>
-      <h1 className='text-5xl text-white'>
-        this is data from backend {data ? data : 'loading...'}
-      </h1>
-      <h2>
-        {data ? data : 'loading...'}
-      </h2>
-      {/* <Routes >
+      <Routes >
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-      </Routes> */}
+      </Routes>
     </div>
   )
 }
